17a28a7ab17f3a8405907fe396e75588d29736b5
Allan Victor <allanvictoravaf@aer.mil.br>
2020-03-18 10:56:29 -0400

    change api

--- api.js
+++ api.js
@@ -1,9 +1,5 @@
-fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
+fetch("https://coronavirus-19-api.herokuapp.com/countries", {
 	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-		"x-rapidapi-key": "fd69e7970bmshe59804d872b3b4fp1d3b19jsn523d63f2198c"
-	}
 })
 .then(response => {
     //console.log(response);
@@ -11,12 +7,11 @@ fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.p
     //document.getElementById("mortes").innerHTML = JSON.parse(response);
 })
 .then(data => {
-    //console.log(data);
-    // document.getElementById("data_country").innerHTML = data.countries_stat[29].country_name
-     document.getElementById("data_cases_actives").innerHTML = data.countries_stat[29].active_cases
-     document.getElementById("data_cases_registered").innerHTML = data.countries_stat[29].cases
-     document.getElementById("data_deaths").innerHTML = data.countries_stat[29].deaths
-     document.getElementById("data_recovered").innerHTML = data.countries_stat[29].total_recovered
+     brazil=data.find(el => el.country ==="Brazil")
+     document.getElementById("data_cases_actives").innerHTML = brazil.active
+     document.getElementById("data_cases_registered").innerHTML = brazil.cases
+     document.getElementById("data_deaths").innerHTML = brazil.deaths
+     document.getElementById("data_recovered").innerHTML = brazil.recovered
 })
 .catch(err => {
 	console.log(err);
