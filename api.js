290e95cde4867dd099e995fef47ca57c0dcf3d3e
Allan Victor <allanvictor@hotmail.com>
2020-03-18 21:41:46 -0400

    remove trash code

--- api.js
+++ api.js
@@ -2,9 +2,7 @@ fetch("https://coronavirus-19-api.herokuapp.com/countries", {
 	"method": "GET",
 })
 .then(response => {
-    //console.log(response);
     return response.json();
-    //document.getElementById("mortes").innerHTML = JSON.parse(response);
 })
 .then(data => {
      brazil=data.find(el => el.country ==="Brazil")
