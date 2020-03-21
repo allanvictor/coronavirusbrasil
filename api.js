ada5e1372660c3ed0f57013bdcdc3b410c551c32
Allan Victor <allanvictor@hotmail.com>
2020-03-21 03:33:25 -0400

    update data each 3 seconds

--- api.js
+++ api.js
@@ -1,16 +1,22 @@
-fetch("https://coronavirus-19-api.herokuapp.com/countries", {
+async function getCoronaData() 
+{
+  let response = await fetch("https://coronavirus-19-api.herokuapp.com/countries", {
 	"method": "GET",
-})
-.then(response => {
-    return response.json();
-})
-.then(data => {
-     brazil=data.find(el => el.country ==="Brazil")
-     document.getElementById("data_cases_actives").innerHTML = brazil.active
-     document.getElementById("data_cases_registered").innerHTML = brazil.cases
-     document.getElementById("data_deaths").innerHTML = brazil.deaths
-     document.getElementById("data_recovered").innerHTML = brazil.recovered
+  })
+
+  let data = await response.json()
+  return data
+}
+
+setInterval(() => {
+getCoronaData()
+  .then(data => {
+    brazil=data.find(el => el.country ==="Brazil")
+    document.getElementById("data_cases_actives").innerHTML = brazil.active
+    document.getElementById("data_cases_registered").innerHTML = brazil.cases
+    document.getElementById("data_deaths").innerHTML = brazil.deaths
+    document.getElementById("data_recovered").innerHTML = brazil.recovered
 })
 .catch(err => {
-	console.log(err);
-});
+   console.log(err)
+})},3000)
