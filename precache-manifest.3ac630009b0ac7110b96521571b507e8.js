52c7640bad02baf207da8c8a950540dfa526ca84
Allan Victor <allanvictor@hotmail.com>
2020-04-13 18:30:56 -0400

    new version with react and a new map

---
+++ precache-manifest.3ac630009b0ac7110b96521571b507e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29542e5d7c5e35c48f8e8dd596853535",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9afb937eefa1ec70193d",
+    "url": "/static/css/main.321a02f7.chunk.css"
+  },
+  {
+    "revision": "da6b749bac819d676de3",
+    "url": "/static/js/2.568fff9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.568fff9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9afb937eefa1ec70193d",
+    "url": "/static/js/main.95079bc4.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  },
+  {
+    "revision": "3c4131b30a6ad085c7c9eaf2e833afe7",
+    "url": "/static/media/brazil_map.3c4131b3.svg"
+  }
+]);
