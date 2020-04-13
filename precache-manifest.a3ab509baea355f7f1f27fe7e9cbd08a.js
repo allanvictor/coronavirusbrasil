237ed09b9beb5f208e678f6b16f20362a13c41ad
Allan Victor <allanvictor@hotmail.com>
2020-04-13 19:48:20 -0400

    Updates

---
+++ precache-manifest.a3ab509baea355f7f1f27fe7e9cbd08a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d301d93f9442ade3e589352d568bf65a",
+    "url": "/coronavirusbrasil/index.html"
+  },
+  {
+    "revision": "478b6638239285903010",
+    "url": "/coronavirusbrasil/static/css/main.321a02f7.chunk.css"
+  },
+  {
+    "revision": "2e7ff19fe774886924f0",
+    "url": "/coronavirusbrasil/static/js/2.d98081cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coronavirusbrasil/static/js/2.d98081cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478b6638239285903010",
+    "url": "/coronavirusbrasil/static/js/main.4df658a7.chunk.js"
+  },
+  {
+    "revision": "ed49409e0606e4a5550f",
+    "url": "/coronavirusbrasil/static/js/runtime-main.98682a3b.js"
+  },
+  {
+    "revision": "3c4131b30a6ad085c7c9eaf2e833afe7",
+    "url": "/coronavirusbrasil/static/media/brazil_map.3c4131b3.svg"
+  }
+]);
